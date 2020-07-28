015ad35bea134268f54f38462e59c98f5edd4a54
xiayu <809700565@qq.com>
2020-07-28 19:33:03 +0800

    yaml2

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.13
 
 require (
 	github.com/kylelemons/go-gypsy v0.0.0-20160905020020-08cad365cd28
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0
 )
--- yaml/yaml2.go
+++ yaml/yaml2.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
 
+type conf struct {
+	Enabled bool   `yaml:"enabled"`
+	Path    string `yaml:"path"`
+}
+
+func (c *conf) getConf() *conf {
+
+	yamlFile, err := ioutil.ReadFile("./yaml/conf.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	return c
+}
+
+func main() {
+	var c conf
+	c.getConf()
+
+	fmt.Println(c)
 }
