a3b1ae6ef1b97461be2e59108c0c8690943030aa
xiayu <809700565@qq.com>
2021-08-26 23:40:46 +0800

    functional

---
+++ functional/fib/fib.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func fibonacci() intGen {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+type intGen func() int
+
+func (i intGen) Read(p []byte) (n int, err error) {
+	next := i()
+	if next > 10000 {
+		return 0, io.EOF
+	}
+	s := fmt.Sprintf("%d\n", next)
+	return strings.NewReader(s).Read(p)
+}
+
+func printContent(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+	f := fibonacci()
+	printContent(f)
+
+}
